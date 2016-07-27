0a80ae1e06ef40f781c49133d70d3e102dad967c
Donato Borrello <donato@jwplayer.com>
2016-07-27 13:22:55 -0400

    First draft of idea for generating user access

---
+++ generate-grants.js
@@ -0,0 +1,35 @@
+let _ = require('lodash');
+let Products = require('./products.json');
+let Users = require('./users.json');
+let Roles = require('./roles.json');
+
+
+function printUserPermissions(grants, db, table) {
+    let txt =
+        '            -\n' +
+        '            grant: ' + grants.join(',') + '\n' +
+        '            database: ' + db + '\n' +
+        '            table: \''+ table +'\'';
+    console.log(txt);
+    return txt;
+}
+    
+
+
+
+console.log('mysql_grants:');
+console.log('    mdb-analytics-vpc-01:');
+_.each(Users, (user, userName) => {
+    console.log('        ' + userName + ':');
+    _.each(user.roles, (roleName) => {
+        _.each(Roles[roleName], (role) => {
+            _.each(role.products, (productName) => {
+                _.each(Products[productName], (product) => {
+                    _.each(product.table, (table) => {
+                        printUserPermissions(role.grant, product.database, table)
+                    });
+                });
+            });
+        });
+    });
+});
